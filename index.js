4ab83460bff1e908eaae8076489fee5a7000ef99
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-06-23 09:18:47 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNPdfScanner } = NativeModules;
+
+export default RNPdfScanner;
---
+++ ios/RNPdfScanner.m
@@ -0,0 +1,13 @@
+
+#import "RNPdfScanner.h"
+
+@implementation RNPdfScanner
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
