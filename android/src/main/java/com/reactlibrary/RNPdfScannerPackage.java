da779c346db75a95208ff156f5e8139d7db00570
Bülent Yalman <blntylmn@gmail.com>
2017-10-22 22:52:27 +0300

    Remove @Override
    this will make compatible with react native 0.47+ and prevents build errors when using 
    "react-native link react-native-document-scanner" as you describe on readme.

--- android/src/main/java/com/reactlibrary/RNPdfScannerPackage.java
+++ android/src/main/java/com/reactlibrary/RNPdfScannerPackage.java
@@ -16,7 +16,6 @@ public class RNPdfScannerPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNPdfScannerModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
